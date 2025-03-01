08f337f107a349d5fe00b265f19a35a8467003c6
Matheus dos Santos <matheus.dso1996@gmail.com>
2025-03-01 12:09:47 -0300

    Basic changes

--- 3-) myfinance-web-dotnet-domain/Entities/PlanoConta.cs
+++ 3-) myfinance-web-dotnet-domain/Entities/PlanoConta.cs
@@ -2,7 +2,7 @@
 
 public class PlanoConta
 {
-    public int? Id {get; set;}
-    public string Descricao {get; set;}
-    public string Tipo {get; set;}
+    public int? Id { get; set;}
+    public string Descricao { get; set;}
+    public string Tipo { get; set;}
 }
