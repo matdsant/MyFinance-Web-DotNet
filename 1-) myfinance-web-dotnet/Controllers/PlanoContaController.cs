85359700b8385c7d5b74d12e52641c90b332afe4
Matheus dos Santos <matheus.dso1996@gmail.com>
2025-03-02 19:06:56 -0300

    Configure "PlanoContaController" and "Cadastrar.cshtml" for form rendering and HTTP methods

--- 1-) myfinance-web-dotnet/Controllers/PlanoContaController.cs
+++ 1-) myfinance-web-dotnet/Controllers/PlanoContaController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using myfinance_web_dotnet.Models;
+using myfinance_web_dotnet_domain.Entities;
 using myfinance_web_dotnet_service.Interfaces;
 
 namespace myfinance_web_dotnet.Controllers
@@ -29,11 +30,13 @@ namespace myfinance_web_dotnet.Controllers
         [Route("Index")]
         public IActionResult Index()
         {
-            var listaPlanoContas =_planoContaService.ListarRegistros();
+            var listaPlanoContas = _planoContaService.ListarRegistros();
             List<PlanoContaModel> listaPlanoContaModel = new List<PlanoContaModel>();
 
-            foreach (var item in listaPlanoContas){
-                var itemPlanoConta = new PlanoContaModel(){
+            foreach (var item in listaPlanoContas)
+            {
+                var itemPlanoConta = new PlanoContaModel()
+                {
                     Id = item.Id,
                     Descricao = item.Descricao,
                     Tipo = item.Tipo
@@ -42,8 +45,54 @@ namespace myfinance_web_dotnet.Controllers
                 listaPlanoContaModel.Add(itemPlanoConta);
             }
             ViewBag.listaPlanoConta = listaPlanoContaModel;
-            
+
             return View();
+        }
+
+        [HttpGet]
+        [Route("Cadastrar")]
+        [Route("Cadastrar/{Id}")]
+        public IActionResult Cadastrar(int? Id)
+        {
+            if (Id != null)
+            {
+                var planoConta = _planoContaService.RetornarRegistro((int)Id);
+                var planoContaModel = new PlanoContaModel()
+                {
+                    Id = planoConta.Id,
+                    Descricao = planoConta.Descricao,
+                    Tipo = planoConta.Tipo
+                };
+
+                return View(planoContaModel);
+            }
+            else{
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [Route("Cadastrar")]
+        [Route("Cadastrar/{Id}")]
+        public IActionResult Cadastrar(PlanoContaModel model)
+        {
+            var planoConta = new PlanoConta()
+            {
+                Id = model.Id,
+                Descricao = model.Descricao,
+                Tipo = model.Tipo
+            };
+
+            _planoContaService.Cadastrar(planoConta);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("Excluir")]
+        public IActionResult Excluir(int? Id)
+        {
+            _planoContaService.Excluir((int)Id);
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
