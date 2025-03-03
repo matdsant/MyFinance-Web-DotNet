f3844ca64e1ed88ce30d966327e8098424d7f795
Matheus dos Santos <matheus.dso1996@gmail.com>
2025-03-02 22:52:31 -0300

    Fixed data rendering issues and improved interaction with the service layer.

--- 1-) myfinance-web-dotnet/Controllers/TransacaoController.cs
+++ 1-) myfinance-web-dotnet/Controllers/TransacaoController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using myfinance_web_dotnet.Models;
 using myfinance_web_dotnet_domain.Entities;
@@ -16,14 +17,17 @@ namespace myfinance_web_dotnet.Controllers
     {
         private readonly ILogger<TransacaoController> _logger;
         private readonly ITransacaoService _transacaoService;
+        private readonly IPlanoContaService _planoContaService;
 
         public TransacaoController(
             ILogger<TransacaoController> logger,
-            ITransacaoService transacaoService
+            ITransacaoService transacaoService,
+            IPlanoContaService planoContaService
             )
         {
             _logger = logger;
             _transacaoService = transacaoService;
+            _planoContaService = planoContaService;
         }
 
         [HttpGet]
@@ -33,8 +37,7 @@ namespace myfinance_web_dotnet.Controllers
             var listaTransacoes = _transacaoService.ListarRegistros();
             List<TransacaoModel> listaTransacaoModel = new List<TransacaoModel>();
 
-            foreach (var item in listaTransacoes)
-            {
+            foreach (var item in listaTransacoes){
                 var itemTransacao = new TransacaoModel(){
                     Id = item.Id,
                     Historico = item.Historico,
@@ -46,7 +49,7 @@ namespace myfinance_web_dotnet.Controllers
 
                 listaTransacaoModel.Add(itemTransacao);
             }
-            ViewBag.listaTransacoes = listaTransacaoModel;
+            ViewBag.ListaTransacao = listaTransacaoModel;
 
             return View();
         }
@@ -56,23 +59,24 @@ namespace myfinance_web_dotnet.Controllers
         [Route("Cadastrar/{Id}")]
         public IActionResult Cadastrar(int? Id)
         {
+            var ListaPlanoContas = new SelectList(_planoContaService.ListarRegistros(), "Id", "Descricao");
+            
+            var itemTransacao = new TransacaoModel(){
+                Data = DateTime.Now,
+                ListaPlanoContas = ListaPlanoContas
+            };
+
             if (Id != null)
             {
                 var transacao = _transacaoService.RetornarRegistro((int)Id);
-                
-                var itemTransacao = new TransacaoModel(){
-                    Id = transacao.Id,
-                    Historico = transacao.Historico,
-                    Data = transacao.Data,
-                    Valor = transacao.Valor,
-                    PlanoContaId = transacao.PlanoContaId
-                };
 
-                return View(itemTransacao);
+                itemTransacao.Id = transacao.Id;
+                itemTransacao.Historico = transacao.Historico;
+                itemTransacao.Data = transacao.Data;
+                itemTransacao.Valor = transacao.Valor;
+                itemTransacao.PlanoContaId = transacao.PlanoContaId;
             }
-            else{
-                return View();
-            }
+            return View(itemTransacao);
         }
 
         [HttpPost]
@@ -83,10 +87,10 @@ namespace myfinance_web_dotnet.Controllers
             var transacao = new Transacao()
             {
                 Id = model.Id,
-                    Historico = model.Historico,
-                    Data = model.Data,
-                    Valor = model.Valor,
-                    PlanoContaId = model.PlanoContaId
+                Historico = model.Historico,
+                Data = model.Data,
+                Valor = model.Valor,
+                PlanoContaId = model.PlanoContaId
             };
 
             _transacaoService.Cadastrar(transacao);
