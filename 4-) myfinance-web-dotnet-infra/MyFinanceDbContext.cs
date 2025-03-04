48d028f40ae2008eec3ed291e7f168f067e4d6d5
Matheus dos Santos <matheus.dso1996@gmail.com>
2025-03-04 20:40:10 -0300

    Azure DB connection example

--- 4-) myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ 4-) myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -10,7 +10,10 @@ public class MyFinanceDbContext : DbContext
     public DbSet<Transacao> Transacao { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
+    {   
+        // Exemplo de Conexão para Banco de Dados Local
         optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=False;");
+        // Exemplo de Conexão para Banco de Dados em Nuvem
+        //optionsBuilder.UseSqlServer(@"Server=yourservename;Database=myfinance;User Id=user;Password=MyPassword");
     }
 }
